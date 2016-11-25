f14f79097c9a84df20a3fdc0f8f942676b970289
aliangel2004 <aliangel2004@gmail.com>
2016-11-25 23:09:45 +0100

    q1 and all indents corrected

--- app/solution2.js
+++ app/solution2.js
@@ -1,21 +1,21 @@
- var quest = {
+var quest = {
 
 findMinMax: function(arry) {
 var max = 0;
 var min = arry[0];
-   for(var i = 0; i < arry.length; i++) {
-     if (arry[i] > max) {
-       max = arry[i];
-     }
-     if (arry[i] < min) {
+  for(var i = 0; i < arry.length; i++) {
+    if (arry[i] > max) {
+      max = arry[i];
+    }
+    if (arry[i] < min) {
        min = arry[i];
-     }
-   }
-   if (min === max) {
-     return [min];
-   } else {
-     return [min, max];
-   }
-}
+    }
+  }
+    if (min === max) {
+      return [min];
+    } else {
+      return [min, max];
+    }
+  }
 }
 module.exports = quest;
--- app/solution3.js
+++ app/solution3.js
@@ -1,19 +1,18 @@
 var findIt = {
-fizzBuzz: function (numb) {
-  if (numb % 3 === 0 && numb % 5 === 0 ) {
-    return 'FizzBuzz'; 
-  } 
-  else if (numb % 3 === 0) {
-    return 'Fizz';
+  fizzBuzz: function (numb) {
+    if (numb % 3 === 0 && numb % 5 === 0 ) {
+      return 'FizzBuzz'; 
+    } 
+    else if (numb % 3 === 0) {
+      return 'Fizz';
+    }
+    else if (numb % 5 === 0) {
+      return 'Buzz';
+    }
+    else {
+      return numb;
+    }
   }
-  else if (numb % 5 === 0) {
-    return 'Buzz';
-  }
-  else {
-    return numb;
-  }
-  
-}
 }
 module.exports = findIt; 
 
