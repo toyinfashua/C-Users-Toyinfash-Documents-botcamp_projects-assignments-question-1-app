c3ece7946bf9b7b9bdf1090651e0e1f1e2d203c5
toyinfashua <oluwatoyinfashua@gmail.com>
2016-11-25 23:36:37 +0100

    commit updated

--- app/solution2.js
+++ app/solution2.js
@@ -1,21 +1,20 @@
 var quest = {
 
-findMinMax: function(arry) {
-var max = 0;
-var min = arry[0];
-  for(var i = 0; i < arry.length; i++) {
-    if (arry[i] > max) {
-      max = arry[i];
+  findMinMax: function(arry) {
+  var max = 0;
+  var min = arry[0];
+    for(var i = 0; i < arry.length; i++) {
+      if (arry[i] > max) {
+        max = arry[i];
+      }
+      if (arry[i] < min) {
+        min = arry[i];
+      }
     }
-    if (arry[i] < min) {
-       min = arry[i];
-    }
-  }
-    if (min === max) {
-      return [min];
-    } else {
-      return [min, max];
-    }
-  }
+      if (min === max) {
+        return [min];
+      } else {
+        return [min, max];
+      }
 }
 module.exports = quest;
